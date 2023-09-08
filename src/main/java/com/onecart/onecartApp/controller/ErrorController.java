4a9e043ef71552f5d33424f66f7b44eee3a4a6af
Susmith A J <susmith508@gmail.com>
2023-09-08 22:02:46 +0530

    first changes

--- src/main/java/com/onecart/onecartApp/controller/ErrorController.java
+++ src/main/java/com/onecart/onecartApp/controller/ErrorController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//@Controller
-//@RequestMapping("/error")
-//public class ErrorController {
-//
-//    @GetMapping
-//    public String error(){
-//        return "error";
-//    }
-//}
+@Controller
+@RequestMapping("/error")
+public class ErrorController {
+
+    @GetMapping
+    public String error(){
+        return "error";
+    }
+}
